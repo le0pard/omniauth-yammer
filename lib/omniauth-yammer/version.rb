86dc13e0fba8839bc6d07d1583f2fd9edd9d9d99
Alexey Vasiliev <leopard.not.a@gmail.com>
2013-03-17 21:58:31 +0200

    yammer domain

--- lib/omniauth-yammer/version.rb
+++ lib/omniauth-yammer/version.rb
@@ -1,5 +1,5 @@
 module OmniAuth
   module Yammer
-    VERSION = "0.0.5"
+    VERSION = "0.0.6"
   end
 end
--- lib/omniauth/strategies/yammer.rb
+++ lib/omniauth/strategies/yammer.rb
@@ -3,17 +3,17 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class Yammer < OmniAuth::Strategies::OAuth2
-      
+
       option :name, 'yammer'
-      
+
       option :client_options, {
-        :site => 'https://www.yammer.com',
+        :site => (ENV['YAMMER_DOMAIN'] || 'https://www.yammer.com'),
         :authorize_url => '/dialog/oauth',
         :token_url => '/oauth2/access_token.json'
       }
-      
+
       uid { raw_info['id'] }
-      
+
       info do
         prune!({
           :nickname => raw_info['name'],
@@ -31,27 +31,27 @@ module OmniAuth
       extra do
         prune!({:raw_info => raw_info})
       end
-      
+
       def request_phase
         options[:response_type] ||= 'code'
         super
       end
-      
+
       def callback_phase
         request.params['state'] = session['omniauth.state']
         super
       end
-        
+
       def build_access_token
         access_token = super
         token = eval(access_token.token)['token']
         @access_token = ::OAuth2::AccessToken.new(client, token, access_token.params)
       end
- 
+
       def raw_info
         @raw_info ||= access_token.get('/api/v1/users/current.json').parsed
       end
-      
+
       private
 
       def prune!(hash)
@@ -60,7 +60,7 @@ module OmniAuth
           value.nil? || (value.respond_to?(:empty?) && value.empty?)
         end
       end
-      
+
       def primary_email
         raw_info['contact']['email_addresses'].detect{|address| address['type'] == 'primary'}['address']
       end
