4bd7aa3091a386d1f2b12b5b2bce82d63cc2a49f
Nikolaos Anastopoulos <ebababi@ebababi.net>
2013-07-12 02:02:05 +0300

    Added some tests based on some other strategies

--- spec/omniauth/strategies/yammer_spec.rb
+++ spec/omniauth/strategies/yammer_spec.rb
@@ -1,7 +1,86 @@
 require 'spec_helper'
 
 describe OmniAuth::Strategies::Yammer do
-  it 'should do some testing' do
-    pending
+  subject { OmniAuth::Strategies::Yammer.new(nil) }
+
+  it 'should add a camelization for itself' do
+    OmniAuth::Utils.camelize('yammer').should == 'Yammer'
+  end
+
+  describe '#client' do
+    it 'has correct GaggleAMP site' do
+      subject.client.site.should eq('https://www.yammer.com')
+    end
+
+    it 'has correct authorize url' do
+      subject.client.options[:authorize_url].should eq('/dialog/oauth')
+    end
+
+    it 'has correct token url' do
+      subject.client.options[:token_url].should eq('/oauth2/access_token.json')
+    end
+  end
+
+  describe '#callback_path' do
+    it 'has the correct callback path' do
+      subject.callback_path.should eq('/auth/yammer/callback')
+    end
+  end
+
+  describe '#uid' do
+    before :each do
+      subject.stub(:raw_info) { { 'id' => 'uid' } }
+    end
+
+    it 'returns the id from raw_info' do
+      subject.uid.should eq('uid')
+    end
+  end
+
+  describe '#info' do
+    context 'and therefore has all the necessary fields' do
+      before :each do
+        subject.stub(:raw_info) { {
+            'full_name' => 'John Doe',
+            'contact' => {
+              'email_addresses' => [
+                { 'type' => 'primary', 'address' => 'john.doe@example.com' }
+              ]
+            },
+            'name' => 'JohnDoe'
+          } }
+      end
+
+      it { subject.info.should have_key :name }
+      it { subject.info.should have_key :email }
+      it { subject.info.should have_key :nickname }
+    end
+
+    context 'and does not fail with empty response' do
+      before :each do
+        subject.stub(:raw_info) { {} }
+      end
+
+      it { expect { subject.info }.not_to raise_error }
+    end
+  end
+
+  describe '#extra' do
+    before :each do
+      subject.stub(:raw_info) { { 'foo' => 'bar' } }
+    end
+
+    it { subject.extra[:raw_info].should eq({ 'foo' => 'bar' }) }
+  end
+
+  describe '#raw_info' do
+    before :each do
+      response = double('response', :parsed => { 'foo' => 'bar' })
+      subject.stub(:access_token) { double('access token', :get => response) }
+    end
+
+    it 'returns parsed response from access token' do
+      subject.raw_info.should eq({ 'foo' => 'bar' })
+    end
   end
 end
